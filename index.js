d2c0ccc36a8a8015a82e3894f4ceab77ef9aea7d
Abhinav Joshi <86758548+Abhinav-J05@users.noreply.github.com>
2022-10-10 21:23:33 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+//Node server
+const io = require('socket.io')(0005, {
+    cors: {
+      origin: '*',
+    }
+  });
+
+  
+const users = {};
+
+io.on('connection', socket =>{
+  socket.on('new-user-joined', name =>{
+    users[socket.id] = name;
+    socket.broadcast.emit('user-joined', name);
+  }); 
+
+  socket.on('send', message =>{
+    socket.broadcast.emit('receive',{message: message, name: users[socket.id]})
+  });
+
+  socket.on('disconnect', message =>{
+    socket.broadcast.emit('left',users[socket.id]);
+    delete users[socket.id];
+  });
+
+})
